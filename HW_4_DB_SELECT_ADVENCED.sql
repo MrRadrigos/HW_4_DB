8519c8b94c3ecca7cea0447b2dc1fd0b555b494c
Anton Eroshkin <121361339+MrRadrigos@users.noreply.github.com>
2023-04-05 23:39:34 +0300

    code improvement

--- HW_4_DB_SELECT_ADVENCED.sql
+++ HW_4_DB_SELECT_ADVENCED.sql
@@ -5,10 +5,9 @@ GROUP BY genre_name
 ORDER BY artist_count DESC;
 
 --Количество треков, вошедших в альбомы 2019–2020 годов.
-SELECT track.track_name, album.year
-FROM album
-LEFT JOIN track on track.album_id = album.id
-WHERE (album.year >= 2019) AND (album.year <= 2020);
+SELECT COUNT(track.id) FROM track /* Количество айди треков из таблицы треков */
+JOIN album ON album.id = album_id  /* Делаем объединение от таблицы треков к альбомам */
+WHERE album.year BETWEEN 2019 AND 2020; /* Где год альбома находится в требуемом промежутке */
 
 --Средняя продолжительность треков по каждому альбому.
 SELECT album_name, AVG(track_lenght) FROM Album
@@ -37,13 +36,13 @@ WHERE artist.artist_name like '%%Lady Gaga%%'
 ORDER BY collection.name;
 
 --Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
-SELECT album.album_name FROM album
-LEFT JOIN artist_album ON album.id = artist_album.album_id
-LEFT JOIN artist ON artist.id = artist_album.artist_id
-LEFT JOIN artist_genre ON artist.id = artist_genre.artist_id
-LEFT JOIN genre ON genre.id = artist_genre.genre_id
-GROUP BY album.album_name
-HAVING COUNT(DISTINCT genre.genre_name) > 1
+SELECT DISTINCT album.album_name FROM album
+JOIN artist_album ON album.id = artist_album.album_id
+JOIN artist ON artist.id = artist_album.artist_id
+JOIN artist_genre ON artist.id = artist_genre.artist_id
+JOIN genre ON genre.id = artist_genre.genre_id
+GROUP BY album.id, artist.id
+HAVING COUNT(DISTINCT genre.id) > 1
 ORDER BY album.album_name;
 
 --Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
@@ -51,22 +50,24 @@ SELECT artist.artist_name, track.track_lenght FROM track
 LEFT JOIN album ON album.id = track.album_id
 LEFT JOIN artist_album ON artist_album.album_id = album.id
 LEFT JOIN artist ON artist.id = artist_album.artist_id
-GROUP BY artist.artist_name, track.track_lenght
-HAVING track.track_lenght = (SELECT MIN(track_lenght) FROM track)
+WHERE track.track_lenght = (
+  SELECT MIN(track_lenght) FROM track
+)
 ORDER BY artist.artist_name;
 
 --Названия альбомов, содержащих наименьшее количество треков.
-SELECT DISTINCT album.album_name FROM album
-LEFT JOIN track ON track.album_id = album.id
-WHERE track.album_id IN (
-    SELECT album_id FROM track
-    GROUP BY album_id
-    HAVING COUNT(id) = (
-        SELECT COUNT(id)
-        FROM track
-        GROUP BY album_id
-        ORDER BY COUNT
-        LIMIT 1
-    )
+SELECT album.album_name FROM album
+LEFT JOIN track ON album.id = track.album_id
+GROUP BY album.id
+HAVING COUNT(track.id) = ( 
+    SELECT COUNT(id) FROM track 
+    GROUP BY album_id 
+    ORDER BY COUNT(id) ASC 
+    LIMIT 1 
 )
-ORDER BY album.album_name;
+ORDER BY album.album_name ASC;
+
+--Наименования треков, которые не входят в сборники.
+SELECT track.track_name FROM track
+LEFT JOIN collection_track ON track.id = collection_track.track_id
+WHERE collection_track.track_id IS NULL;
